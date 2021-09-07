4351fcbd9760106f74c0acc98e5ae2e6549c05b0
gajendrapratap <gajendrapratap84@gmail.com>
2021-09-07 18:16:29 +0530

    added save functionality

--- src/app/new-prod-temp/new-prod-temp.component.ts
+++ src/app/new-prod-temp/new-prod-temp.component.ts
@@ -12,6 +12,9 @@ export class NewProdTempComponent implements OnInit {
   selectedDay;
   attributesGroupAttributes;
   addNewAttribute;
+  static templateCounter=1;
+  templateName;
+  selectedProductType;
   constructor() {}
 
   ngOnInit(): void {
@@ -64,9 +67,42 @@ export class NewProdTempComponent implements OnInit {
 
   selectChangeHandler(event: any) {
     this.selectedDay = event.target.value;
+    console.log(event.target.value);
+    
     const attrbs = this.attributeGroup.find(
-      (d) => d.description === event.target.value
+      (d) => d.id.toString() === event.target.value.toString()
     );
     this.attributesGroupAttributes = attrbs.attributesGroupAttributes;
+  }
+  selectProductTypeChangeHandler(event: any) {
+    this.selectedProductType = event.target.value;
+    
+  }
+
+  postNewTemplate(payload) {
+    axios
+      .post(
+        'https://dadyin-product-server-7b6gj.ondigitalocean.app/api/product_templates/',
+        payload
+      )
+      .then((response) => {
+        console.log('success',response);
+        
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .then(function () {});
+  }
+
+  onSave(event: any) {
+    const templateName = 'New Tempalate hexode' + NewProdTempComponent.templateCounter;
+    NewProdTempComponent.templateCounter++;
+
+   this.postNewTemplate({
+     description:this.templateName,
+     product_type: this.selectedProductType,
+     product_template_attributes: this.attributesGroupAttributes
+    });
   }
 }
