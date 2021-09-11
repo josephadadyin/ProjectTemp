51fac6db8d9f05f3d9e94a4aa200943718c74fbf
narendra-2605 <narendra.vema@ieee.org>
2021-09-11 15:12:36 +0530

    Get Api Intigration With Add new product

--- src/app/product-table/product-table.component.ts
+++ src/app/product-table/product-table.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import axios from 'axios';
 
 @Component({
   selector: 'app-product-table',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductTableComponent implements OnInit {
   tables = [1];
+  AddProductData;
+  xproductName = [];
+  selectProductName;
+  xselectCost = '';
+  xselectDensity = '';
+  ConversionName;
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.AddProduct();
+    this.Conversion();
+  }
   public fieldArray: Array<any> = [];
   public newAttribute: any = {};
   addFieldValue() {
@@ -21,5 +31,75 @@ export class ProductTableComponent implements OnInit {
   addFieldValue1() {
     this.fieldArray1.push(this.newAttribute1);
     this.newAttribute1 = {};
+  }
+
+  // ======================= Product Get API ======================================
+
+  AddProduct() {
+    axios
+      .get(
+        'https://dadyin-product-server-7b6gj.ondigitalocean.app/api/products/'
+      )
+      .then((response) => {
+        this.AddProductData = response.data.results;
+        this.xproductName = this.AddProductData.map((d) => ({
+          description: d.description,
+          id: d.id,
+        }));
+        // console.log(this.addNewProduct);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .then(function () {});
+  }
+
+  getxPrice(description) {
+    const selectedProduct = this.AddProductData.find(
+      (d) => d.description === description
+    );
+    this.xselectCost = selectedProduct.productAttributeValues.find(
+      (d) => d.attribute.description === 'Cost'
+    )['attributeValue'];
+  }
+
+  getxDensity(description) {
+    const selectedProduct = this.AddProductData.find(
+      (d) => d.description === description
+    );
+    this.xselectDensity = selectedProduct.productAttributeValues.find(
+      (d) => d.attribute.description === 'Density'
+    )['attributeValue'];
+  }
+
+  selectProductNameChangeHandler(event: any) {
+    this.selectProductName = event.target.value;
+    console.log(event.target.value);
+    this.getxDensity(event.target.value);
+    this.getxPrice(event.target.value);
+    this.getxPrice(event.tar);
+    this.getxDensity(event.tar);
+  }
+  addData() {
+    const selectedProduct = this.xproductName[0];
+    this.getxPrice(selectedProduct.description);
+    this.getxDensity(selectedProduct.description);
+  }
+
+  // ========================== Conversion Cost API ===================
+
+  Conversion() {
+    axios
+      .get(
+        'https://dadyin-product-server-7b6gj.ondigitalocean.app/api/conversion_types/'
+      )
+      .then((response) => {
+        this.ConversionName = response.data.results;
+        // console.log(this.conversionName);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .then(function () {});
   }
 }
